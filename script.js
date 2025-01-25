692ec5830583c9aa83d5ac12c0b7f57403144509
Aman Thapliyal <thapliyalaman699@gmail.com>
2025-01-25 09:22:06 +0530

    Added the education section with media queries

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function toggleMenu() {
     navbar.classList.toggle('nav-toggle');
 }
 
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["frontend development", "web designing", "web development"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
+// <!-- typed js effect ends -->
